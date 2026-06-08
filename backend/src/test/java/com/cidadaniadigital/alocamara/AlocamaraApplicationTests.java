c19b795dd43df5bb652d4304fdf103e56d47727a
Gustavo-HDM <comercial@innovatechais.com>
2026-06-08 17:16:20 -0300

    chore: move frontend to /frontend folder

---
+++ backend/src/test/java/com/cidadaniadigital/alocamara/AlocamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadaniadigital.alocamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
